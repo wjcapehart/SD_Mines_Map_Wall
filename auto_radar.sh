f24a7ecf6acb562b22949e9d53cdd1d72c3152aa
Bill Capehart <William.Capehart@sdsmt.edu>
2022-01-29 17:15:31 -0700

    Update auto_radar.sh

--- auto_radar.sh
+++ auto_radar.sh
@@ -11,10 +11,12 @@ echo
 echo "Entering Working Directory"
 echo
 cd /projects/SD_Mines_Map_Wall
+rm -frv  /projects/SD_Mines_Map_Wall/radar_temp_files/*.png
+rm -frv  /projects/SD_Mines_Map_Wall/radar_temp_files/*.txt
 echo
 echo  "Firing Things Up!"
 echo
-. source /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/Radar_and_Metars.py
+. source /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/Radar_and_Metars.py > /projects/SD_Mines_Map_Wall/__log_radar_${DATESTRING}.log 2>&1
 echo
 echo "Ending Script"
 echo
