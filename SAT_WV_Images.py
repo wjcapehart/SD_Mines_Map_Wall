a40366d7995bcd3d71e8561586d30f0aa836148d
Bill Capehart <William.Capehart@sdsmt.edu>
2022-01-30 15:56:57 -0700

    fixfix water vapor scene

--- SAT_WV_Images.py
+++ SAT_WV_Images.py
@@ -1,38 +1,60 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Water Vapour
+# # Water Vapor
 
-# In[30]:
+# In[ ]:
 
 
-from datetime import datetime
+##################################################
+#
+# Libraries
+#
+
+
+from metpy.plots    import colortables
+from metpy.plots    import add_timestamp
+from datetime       import datetime
 from siphon.catalog import TDSCatalog
-from datetime import datetime
-import metpy
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-import matplotlib.pyplot as plt
-from metpy.plots import colortables
-from metpy.plots import add_timestamp
+from datetime       import datetime
+
 import numpy             as np
 import os                as os
-import pandas             as pd
+import pandas            as pd
+import pathlib           as pathlib
+
+import metpy
+import cartopy.crs       as ccrs
+import cartopy.feature   as cfeature
+import matplotlib.pyplot as plt
 
 
-# In[31]:
+#
+##################################################
 
+
+# In[ ]:
+
+
+##################################################
+#
+# Control Setup
+#
 
 # %load solutions/data_url.py
+
 total_frames = 45*2
 
-processing_directory = "./sat_wv_temp_files/"
+png_processing_directory = "./temp_files_sat_wv/"
 
+gif_file_name = "./graphics_files/RealTime_SAT_IR_Loop.gif"
 
+image_header_label = "GOES 16 Band 8 [6.2 µm Upper-level Water Vapor]"
 
 # Cell content replaced by load magic replacement.
 
 # Create variables for URL generation
+
 image_date = datetime.utcnow().date()
 region = 'CONUS'
 channel = 8
@@ -41,20 +63,33 @@ channel = 8
 # https://thredds-test.unidata.ucar.edu/thredds/catalog/satellite/goes16/GOES16/Mesoscale-1/Channel08/20181113/catalog.html
 
 # Construct the data_url string
+
 data_url = ('https://thredds.ucar.edu/thredds/catalog/satellite/goes/east/products/'
             f'CloudAndMoistureImagery/{region}/Channel{channel:02d}/current/catalog.xml')
 
 # Print out your URL and verify it works!
+
 print(data_url)
 
+#
+##################################################
 
-# In[40]:
 
+# In[ ]:
+
+
+##################################################
+#
+# Pull Catalog
+#
 
 cat = TDSCatalog(data_url)
 
+#
+##################################################
 
-# In[45]:
+
+# In[ ]:
 
 
 ##################################################
@@ -66,7 +101,7 @@ file_names_to_retain = list()
 file_names_to_use    = list()
 
 
-for i in range(1,len(cat.datasets[0:total_frames])+1,1) : 
+for i in range(0,len(cat.datasets[0:total_frames])+1,1) : 
     filename = png_processing_directory + cat.datasets[i].name.replace(".nc",".png")
     file_names_to_retain.append(filename)
     file_names_to_use.append(filename)
@@ -77,6 +112,7 @@ files_on_hand = [png_processing_directory + s for s in os.listdir(png_processing
 file_names_to_retain.sort()
 file_names_to_use.sort()
 
+
 #
 ##################################################    
 
@@ -84,60 +120,86 @@ file_names_to_use.sort()
 # In[ ]:
 
 
+##################################################
+#
+# Clean PNG Directory
+#
+
+for filename in files_on_hand:
+    print(filename)
+    if filename not in file_names_to_retain:
+        print("Purging ", filename )
+        os.remove( filename  )
+    else:
+        print("Keeping ", filename )
+#
+##################################################    
 
 
+# In[ ]:
 
-# In[34]:
 
+##################################################
+#
+# Create PNGs
+#
 
-for i in range(1,len(cat.datasets[1:total_frames])+1,1) : 
+for i in range(0,len(cat.datasets[0:total_frames])+1,1) : 
 
     dataset = cat.datasets[i]
     
-    print(dataset.name.replace(".nc",".png")
-    ds = dataset.remote_access(use_xarray=True)
-    dat = ds.metpy.parse_cf('Sectorized_CMI')
-    proj = dat.metpy.cartopy_crs
-    x = dat['x']
-    y = dat['y']
+    dataset_png_file_name = png_processing_directory + dataset.name.replace(".nc", ".png")
+    
+    if (not pathlib.Path(dataset_png_file_name).is_file() ):
+
+        ds = dataset.remote_access(use_xarray=True)
+        dat = ds.metpy.parse_cf('Sectorized_CMI')
+        proj = dat.metpy.cartopy_crs
+        x = dat['x']
+        y = dat['y']
 
 
-    tz='America/Denver'
-    time_utc = datetime.strptime(ds.start_date_time, '%Y%j%H%M%S')
-    valid_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").strftime("%Y-%m-%d %H:%M:%S %Z")
-    local_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").tz_convert(tz=tz).strftime("%Y-%m-%d %H:%M:%S %Z")
+        tz='America/Denver'
+        time_utc   = datetime.strptime(ds.start_date_time, '%Y%j%H%M%S')
+        valid_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").strftime("%Y-%m-%d %H:%M:%S %Z")
+        local_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").tz_convert(tz=tz).strftime("%Y-%m-%d %H:%M:%S %Z")
 
-    file_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").strftime("%Y-%m-%d_%H%M")
-
-
-    print(valid_time,local_time)
+        file_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").strftime("%Y-%m-%d_%H%M")
 
 
+        print(valid_time,local_time)
 
-    fig = plt.figure(figsize=(13, 8),
-                           facecolor = 'white')
 
-    plt.suptitle("GOES 16 Band 8 [6.2 µm Upper-level Water Vapor]",
-                    fontsize=20, color="black")
-    ax = fig.add_subplot(1, 1, 1, projection=proj)
-    ax.set_title(valid_time + "  (" + local_time+")",
-                    fontsize=15, color="black")
-    ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=2)
-    ax.add_feature(cfeature.STATES.with_scale('50m'), linestyle=':', edgecolor='black')
-    ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=2, edgecolor='black')
 
-    im = ax.imshow(dat, extent=(x.min(), x.max(), y.min(), y.max()), origin='upper')
+        fig = plt.figure(figsize=(13, 8), facecolor = 'white')
 
-    wv_norm, wv_cmap = colortables.get_with_range('WVCIMSS_r', 195, 265)
-    im.set_cmap(wv_cmap)
-    im.set_norm(wv_norm)
-    plt.tight_layout()
-    plt.savefig("./sat_wv_temp_files/Sat_WV_Loop_Image_"+file_time+".png")
-    plt.close()
+        plt.suptitle(image_header_label,
+                     fontsize = 20, 
+                     color    = "black")
+        ax = fig.add_subplot(1, 1, 1, projection=proj)
+        ax.set_title(valid_time + "  (" + local_time+")",
+                        fontsize=15, color="black")
+        ax.add_feature(cfeature.COASTLINE.with_scale('50m'), linewidth=2)
+        ax.add_feature(cfeature.STATES.with_scale('50m'),    linestyle=':', edgecolor='black')
+        ax.add_feature(cfeature.BORDERS.with_scale('50m'),   linewidth=2, edgecolor='black')
+
+        im = ax.imshow(dat, extent=(x.min(), x.max(), y.min(), y.max()), origin='upper')
+
+        wv_cmap = colortables.get_colortable('WVCIMSS_r')
+        im.set_cmap(wv_cmap)
+        plt.tight_layout()
+
+        plt.savefig( dataset_png_file_name)
+        plt.close()
+    else:
+        print("We already have this one!")
     
 
+#
+##################################################
 
-# In[35]:
+
+# In[ ]:
 
 
 ##################################################
@@ -145,11 +207,12 @@ for i in range(1,len(cat.datasets[1:total_frames])+1,1) :
 # Convert PNGs into an Animated GIF
 #
 
-os.system("convert -delay 15 " + 
-          "./sat_wv_temp_files/Sat_WV_Loop_Image_*.png"  + 
-          " " + 
-          "./graphics_files/RealTime_SAT_WV_Loop.gif")
+big_string = " ".join(file_names_to_use)
 
+os.system("convert -delay 15 " + 
+          big_string + 
+          " " + 
+          gif_file_name)
 
 #
 ##################################################
