a6aec08b9653b94ace0908f9807e791d678ffc70
Bill Capehart <William.Capehart@sdsmt.edu>
2022-02-21 10:41:56 -0700

    NWS_Warnings Now from NOAA_SDK

--- NWS_Warnings.py
+++ NWS_Warnings.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# NWS Warning Board
+# # NWS Warning Polygons
 
 # In[ ]:
 
 
-from awips.dataaccess import DataAccessLayer
-from awips.tables import vtec
-from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
+#Import the necessary packages
+import datetime as dt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-from cartopy.feature import ShapelyFeature,NaturalEarthFeature
-from shapely.geometry import MultiPolygon,Polygon
-import pyproj as pyproj
-import pandas as pd
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import noaa_sdk as noaa_sdk
+import pandas as pd
+import shapely as shapely
 from   metpy.plots       import  USCOUNTIES
 
 
+import geopandas as gp
+
+
 # In[ ]:
+
+
+
+
+
+# In[2]:
 
 
 proj_data_text = '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs'
@@ -35,17 +40,12 @@ myproj = ccrs.AlbersEqualArea(central_longitude=-96,
                               false_easting=0.0, 
                               false_northing=0.0, 
                               standard_parallels=(29.5, 45.5))
-def warning_color(phensig):
-    return vtec[phensig]['color']
 
-
-
-# In[ ]:
 
 
 tz='America/Denver'
 
-time_utc = datetime.utcnow()
+time_utc = dt.datetime.utcnow()
 valid_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").strftime("%Y-%m-%d %H%M %Z")
 local_time = pd.to_datetime(time_utc).tz_localize(tz="UTC").tz_convert(tz=tz).strftime("%Y-%m-%d %H%M %Z")
 
@@ -53,118 +53,142 @@ print(valid_time)
 print(local_time)
 
 
-# In[ ]:
+# In[3]:
 
 
-DataAccessLayer.changeEDEXHost("edex-cloud.unidata.ucar.edu")
-request = DataAccessLayer.newDataRequest()
-request.setDatatype("warning")
-request.setParameters('phensig')
-times   = DataAccessLayer.getAvailableTimes(request)
+# Open the NWS API in python to get the active alerts
+n = noaa_sdk.noaa.NOAA()
+alerts = n.active_alerts()
+
+
+# In[4]:
+
+
+#injest //shapefiles/CONUS_UGC_Zones/
+
+
+UGC_Zones_Shapefile    = gp.read_file('./shapefiles/CONUS_UGC_Zones/CONUS_UGC_Zones.shp')
+UGC_Counties_Shapefile = gp.read_file('./shapefiles/CONUS_UGC_Counties/CONUS_UGC_Counties.shp')
+ 
+UGC_Shapefile = UGC_Zones_Shapefile.append(UGC_Counties_Shapefile).drop(["UGC_Temp",
+                                                                         "Shape_Leng",
+                                                                         "Shape_Le_1",
+                                                                         "Shape_Area",
+                                                                         "STATE",
+                                                                         "CWA",
+                                                                         "COUNTYNAME",
+                                                                         "FIPS",
+                                                                         "TIME_ZONE",
+                                                                         "FE_AREA"],
+                                                                       axis = 'columns').reset_index(drop=True)  
+    
+UGC_Zone_County_List = UGC_Shapefile['UGC'].to_list()
+
+
+# In[6]:
+
+
+# priority warnings table
+warning_priority_table = pd.read_csv("./warning_table_sorted.csv")
+
+warning_priority_table = warning_priority_table.rename(columns={"hdln": "event"})
+
+
+# In[7]:
 
 
 
-# In[ ]:
+current_warnings = pd.DataFrame(columns = ['event',
+                                           'UGC',
+                                           'coverage',
+                                           'message'])
 
 
 
-# Get records for last 50 available times
-response = DataAccessLayer.getGeometryData(request, times[-50:-1])
-print("Using " + str(len(response)) + " records")
-
-
-# In[ ]:
-
-
-# Each record will have a numpy array the length of the number of "parameters"
-# Default is 1 (request.setParameters('phensig'))
-parameters = {}
-for x in request.getParameters():
-    parameters[x] = np.array([])
-
-
-# In[ ]:
+i = 0
 
 
 
-current_warnings = pd.DataFrame(columns = ['hdln',
-                                           'phensigString',
-                                           'site',
-                                           'ref',
-                                           'color'])
+for alert in alerts['features']:
+    event = alert['properties']['event']
+    if (event != "Test Message"): 
 
-siteids=np.array([])
-periods=np.array([])
-reftimes=np.array([])
-
-
-for ob in response:
-
-    site = ob.getLocationName()
-    per  = ob.getDataTime().getValidPeriod()
-    ref  = ob.getDataTime().getRefTime()
-
-    # do not plot if phensig is blank (SPS)
-    if (1>0) : #ob.getString('phensig'):
-
-        phensigString = ob.getString('phensig')
-
-        siteids = np.append(siteids,site)
-        periods = np.append(periods,per)
-        reftimes = np.append(reftimes,ref)
-
-        for parm in parameters:
-            parameters[parm] = np.append(parameters[parm],ob.getString(parm))
-
-  
-
-        if (0 > 1) : 
-            print(vtec[phensigString]['hdln']
-              + " (" + phensigString + ") issued at " + str(ref)
-              + " ("+str(poly.geom_type) + geom_count + ")")
-
-        color = warning_color(phensigString)
-
+        ugc_codes = alert['properties']['geocode']['UGC']
+        message   = alert['properties']['@id']
         
-        deleteme = pd.DataFrame([[vtec[phensigString]['hdln'],
-                                  phensigString,
-                                  site,
-                                  ref,
-                                  color]], 
-                                  columns = ['hdln',
-                                           'phensigString',
-                                           'site',
-                                           'ref',
-                                           'color'])
-        
-        current_warnings = pd.concat([current_warnings, deleteme]) 
-        
-current_warnings = current_warnings.reset_index().drop(columns=["index"])
+        for ugc_code in ugc_codes:
+            if ugc_code in UGC_Zone_County_List: 
+                if (ugc_code[2] == 'C'):
+                    coverage = "County"
+                else:
+                    coverage = "Zones"
+
+                deleteme = pd.DataFrame([[event,
+                          ugc_code,
+                          coverage,
+                          message]], 
+                          columns = ['event',
+                                     'UGC',
+                                     'coverage',
+                                     'message'])
+                current_warnings = pd.concat([current_warnings, 
+                                              deleteme]) 
+
+ 
+
+                i = i + 1
+    
+current_warnings = current_warnings.merge(warning_priority_table, how='left', on='event')
+current_warnings = UGC_Shapefile.merge(current_warnings, how='right', on='UGC')
+current_warnings = current_warnings.sort_values("Rank", ascending=False)
+
+warning_color_table = current_warnings[["event","color"]].drop_duplicates()
 
 
-current_warnings.to_excel("./Current_Warnings.xlsx")
+current_warnings.drop(["geometry"], axis="columns").to_excel("Current_Warnings.xlsx")
 
-warning_color_table = current_warnings.drop(columns=["phensigString","site","ref"]).drop_duplicates()
+print("done: ",i,"rows; ",len(warning_color_table),"event types")
+
+
+# In[8]:
 
 
 legend_color_table = warning_color_table.values.tolist()
-legend_color_table
-
-
 
 legend_color_table = []
 
 for row in warning_color_table.iterrows():
     mypatch = [mpatches.Patch(color=row[1][1], label=row[1][0])]
     legend_color_table = legend_color_table + mypatch
-    
   
 
 
 # In[ ]:
 
 
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
 bbox=[-120,-73,22.5,50]
+
+xxx = [-120.0, -73.0]
+yyy = [  22.5,  50.0]
+
 
 fig = plt.figure(figsize   = (11, 6), 
                  facecolor = 'white')
@@ -183,70 +207,16 @@ ax.set_title(valid_time + "  (" + local_time+")",
              fontsize=15, 
              color="black")
 
-
-
-
-
-siteids=np.array([])
-periods=np.array([])
-reftimes=np.array([])
-
-first = True
-
-for ob in response:
-
-    poly = ob.getGeometry()
-    site = ob.getLocationName()
-    per  = ob.getDataTime().getValidPeriod()
-    ref  = ob.getDataTime().getRefTime()
-
-    # do not plot if phensig is blank (SPS)
-    if (1 > 0) : #ob.getString('phensig'):
-
-        phensigString = ob.getString('phensig')
-
-        siteids = np.append(siteids,site)
-        periods = np.append(periods,per)
-        reftimes = np.append(reftimes,ref)
-
-        for parm in parameters:
-            parameters[parm] = np.append(parameters[parm],ob.getString(parm))
-
-        if poly.geom_type == 'MultiPolygon':
-            geometries = np.array([])
-            geometries = np.append(geometries,MultiPolygon(poly))
-            geom_count = ", " + str(len(geometries)) +" geometries"
-        else:
-            geometries = np.array([])
-            geometries = np.append(geometries,Polygon(poly))
-            geom_count=""
-
-        for geom in geometries:
-            bounds = Polygon(geom)
-            intersection = bounds.intersection
-            geoms = (intersection(geom)
-                 for geom in geometries
-                 if bounds.intersects(geom))
-
-        if (0 > 1) : 
-            print(vtec[phensigString]['hdln']
-              + " (" + phensigString + ") issued at " + str(ref)
-              + " ("+str(poly.geom_type) + geom_count + ")")
-
-        color = warning_color(phensigString)
-        shape_feature = ShapelyFeature(geoms,ccrs.PlateCarree(),
-                        facecolor=color, edgecolor=color)
-        ax.add_feature(shape_feature)
-
+current_warnings.plot(ax = ax,aspect='equal',
+                      facecolor=current_warnings["color"],
+                      transform=ccrs.PlateCarree(),
+                      edgecolor='None', linewidth=0)
 
 
 ax.add_feature(cfeature.COASTLINE.with_scale('50m'), 
                linewidth = 0.5)
 ax.add_feature(cfeature.STATES.with_scale('50m'),    
                linewidth = 0.25, 
-               edgecolor = 'black')
-ax.add_feature(cfeature.BORDERS.with_scale('50m'),   
-               linewidth = 0.5, 
                edgecolor = 'black')
 ax.add_feature(cfeature.LAKES.with_scale('50m'),   
                linewidth = 0.5,
@@ -258,34 +228,25 @@ ax.add_feature(feature    = USCOUNTIES,
                    facecolor  = 'none')
 
 
-plt.subplots_adjust(left=.01, 
-                        right=.78, 
-                        top=0.91, 
-                        bottom=.01)
+ax.set_frame_on(False)
 
-fig.legend(handles=legend_color_table, loc='right',frameon=False)
+plt.subplots_adjust(left   = 0.01, 
+                    right  = 0.75, 
+                    top    = 0.91, 
+                    bottom = 0.01)
 
 
-#plt.tight_layout()
 
+fig.legend(handles = legend_color_table, 
+           loc     = 'right',
+           frameon = False)
 plt.savefig(gif_file_name)
+
 
 plt.close()
 
 
 print("done")
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
