a2f1cf7ab707f956f48d2aed7398c6fbf4a30e38
Bill Capehart <William.Capehart@sdsmt.edu>
2024-02-20 11:16:09 -0700

    Going back to the NWS Forecast Map

--- auto_nws_wx_map.sh
+++ auto_nws_wx_map.sh
@@ -15,7 +15,8 @@ rm -frv /projects/SD_Mines_Map_Wall/temp_sfc_analysis/*
 echo
 echo  "Firing Things Up!"
 echo
-.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/Surface_Analysis_Rapid_Refresh.py > /projects/SD_Mines_Map_Wall/__log_nws_map.log 2>&1
+.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/NWS_Wx_Map.py > /projects/SD_Mines_Map_Wall/__log_nws_map.log 2>&1
+#.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/Surface_Analysis_Rapid_Refresh.py > /projects/SD_Mines_Map_Wall/__log_nws_map.log 2>&1
 echo
 echo "Ending Script"
 echo
