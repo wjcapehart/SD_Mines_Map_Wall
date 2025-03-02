aab64e5d5a0fdd5240c40dd134d227a5fab8e600
Bill Capehart <William.Capehart@sdsmt.edu>
2025-03-01 19:12:02 -0700

    Update auto_warnings.sh

--- auto_warnings.sh
+++ auto_warnings.sh
@@ -14,7 +14,7 @@ cd /projects/SD_Mines_Map_Wall
 echo
 echo  "Firing Things Up!"
 echo
-.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/NWS_Warnings.py > /projects/SD_Mines_Map_Wall/__log_warnings.log 2>&1
+.  /home/wjc/.bashrc ; $CONDA_PYTHON_EXE /projects/SD_Mines_Map_Wall/NWS_Warnings.py > /projects/SD_Mines_Map_Wall/__log_warnings.log 2>&1
 echo
 echo "Ending Script"
 echo
