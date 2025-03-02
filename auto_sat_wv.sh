94d2da05f405ac48de590601baafcacd46ec400d
Bill Capehart <William.Capehart@sdsmt.edu>
2025-03-01 19:10:13 -0700

    Update auto_sat_wv.sh

--- auto_sat_wv.sh
+++ auto_sat_wv.sh
@@ -14,7 +14,7 @@ cd /projects/SD_Mines_Map_Wall
 echo
 echo  "Firing Things Up!"
 echo
-.  /home/wjc/.bashrc ; /home/wjc/miniconda3/bin/python /projects/SD_Mines_Map_Wall/SAT_WV_Images.py > /projects/SD_Mines_Map_Wall/__log_sat_wv.log 2>&1
+.  /home/wjc/.bashrc ; $CONDA_PYTHON_EXE /projects/SD_Mines_Map_Wall/SAT_WV_Images.py > /projects/SD_Mines_Map_Wall/__log_sat_wv.log 2>&1
 echo
 echo "Ending Script"
 echo
